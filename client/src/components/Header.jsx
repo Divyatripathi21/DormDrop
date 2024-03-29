59a71fb545707bc3238e4ffd7e1dedb5226ce5ab
Divya prakash tripathi <divytripathidivy@gmail.com>
2024-03-29 15:49:29 +0530

    created home page ui signout functionality and added dropdown in header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,41 +1,93 @@
-import { Button, Navbar, TextInput } from 'flowbite-react';
-import { Link, useLocation } from 'react-router-dom';
-import { AiOutlineSearch } from 'react-icons/ai';
-import { FaMoon } from 'react-icons/fa';
+import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
+import { Link, useLocation } from "react-router-dom";
+import { FaMoon } from "react-icons/fa";
+import { useDispatch, useSelector } from "react-redux";
+
+import {
+  signout
+} from '../redux/user/userSlice';
 
 export default function Header() {
-    const path = useLocation().pathname;
+  const path = useLocation().pathname;
+  const { currentUser } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+
+
+  const handleSignout = async () => {
+    try {
+      const res = await fetch('/api/auth/signout', {
+        method: 'POST',
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signout());
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
-    <Navbar className='border-b-2 px-0 lg:px-0'>
+    <Navbar className="border-b-2">
       <Link
-        to='/'
-        className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
+        to="/"
+        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
       >
-        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
           Dorm
         </span>
         Drop
       </Link>
-      
-      
-      <div className='flex gap-2 md:order-2'>
-        <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
+
+      <div className="flex gap-2 md:order-2">
+        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
           <FaMoon />
         </Button>
-        <Link to='/signup'>
-          <Button gradientDuoTone='purpleToBlue'>Sign-In/Up</Button>
-        </Link>
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={<Avatar alt="user" rounded />}
+          >
+            <Dropdown.Header>
+              <span className="block text-sm">@{currentUser.username}</span>
+              <span className="block text-sm font-medium truncate">
+                {currentUser.email}
+              </span>
+            </Dropdown.Header>
+            <Link to={"/dashboard?tab=profile"}>
+              <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider />
+            <Dropdown.Item onClick={handleSignout}>Sign out</Dropdown.Item>
+          </Dropdown>
+        ) : (
+          <Link to="/signin">
+            <Button gradientDuoTone="purpleToBlue" outline>
+              Sign in/up
+            </Button>
+          </Link>
+        )}
         <Navbar.Toggle />
       </div>
+
       <Navbar.Collapse>
-        <Navbar.Link active={path === "/"} as={'div'}>
-          <Link to='/' className='text-lg'>Home</Link>
+        <Navbar.Link active={path === "/"} as={"div"}>
+          <Link to="/" className="text-lg">
+            Home
+          </Link>
         </Navbar.Link>
-        <Navbar.Link active={path === "/about"} as={'div'}>
-          <Link to='/about' className='text-lg'>About</Link>
+        <Navbar.Link active={path === "/about"} as={"div"}>
+          <Link to="/about" className="text-lg">
+            About
+          </Link>
         </Navbar.Link>
-        <Navbar.Link active={path === "/projects"} as={'div'}>
-          <Link to='/contactus' className='text-lg'>ContactUs</Link>
+        <Navbar.Link active={path === "/contactus"} as={"div"}>
+          <Link to="/contactus" className="text-lg">
+            Contact Us
+          </Link>
         </Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
